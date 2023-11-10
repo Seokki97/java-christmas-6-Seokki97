4df7274a95d3db46ee22bb924180e3c702760599
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-11 04:44:40 +0900

    feat - 메뉴 타입이 동일한 것의 개수 찾기 구현

--- src/main/java/christmas/repository/MenuRepository.java
+++ src/main/java/christmas/repository/MenuRepository.java
@@ -1,5 +1,6 @@
-package christmas.controller.repository;
+package christmas.repository;
 
+import christmas.domain.Order;
 import java.util.List;
 
 public enum MenuRepository {
@@ -11,7 +12,7 @@ public enum MenuRepository {
     BBQ("메인", "바비큐립", 54000),
     SEA_PASTA("메인", "해산물파스타", 35000),
     CHRISTMAS_PASTA("메인", "크리스마스파스타", 25000),
-    CHOCO_CAKE("디저트", "초코케이트", 15000),
+    CHOCO_CAKE("디저트", "초코케이크", 15000),
     ICE_CREAM("디저트", "아이스크림", 5000),
     ZERO_COKE("음료", "제로콜라", 3000),
     RED_WINE("음료", "레드와인", 60000),
@@ -36,6 +37,16 @@ public enum MenuRepository {
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."));
     }
 
+    public static int countMenuTypeEquals(String type){
+        List<MenuRepository> menuList = List.of(MenuRepository.values());
+
+        return (int) menuList.stream()
+                .filter(item -> item.menuType.equals(type))
+                .count();
+    }
+    public boolean isSameType(String type){
+        return menuType.equals(type);
+    }
     public int getPrice() {
         return price;
     }
