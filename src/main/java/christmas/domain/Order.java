37f5b540ade4b1b21d73312a22ad9dae46adeb7a
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-11 23:03:04 +0900

    feat - 가격 계산 기능 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,14 +1,19 @@
 package christmas.domain;
 
 
-import christmas.controller.repository.MenuRepository;
+import christmas.repository.MenuRepository;
+import java.util.List;
 
 public record Order(MenuRepository orderItem, int orderCount) {
-
 
     @Override
     public String toString() {
         return orderItem.getName() + " " + orderCount + "개";
     }
+
+    public int calculatePrice() {
+        return orderItem.getPrice() * orderCount;
+    }
+
 
 }
