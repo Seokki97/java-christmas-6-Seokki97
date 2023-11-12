d71834ea2ff830140c3ef6593a70e804a4675927
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-13 01:34:56 +0900

    feat - 주문 내역에 중복된 항목 있을 경우 예외

--- src/main/java/christmas/service/InputValidator.java
+++ src/main/java/christmas/service/InputValidator.java
@@ -1,5 +1,9 @@
 package christmas.service;
 
+import christmas.dto.OrderRequest;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class InputValidator {
@@ -17,21 +21,33 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
-
-    public void validateOrderMenuIsOverZero(String orderMenu){
-        if(!orderMenu.contains(",")){
+    public void validateInputtedOrderData(String orderMenu){
+        validateOrderMenuIsOverZero(orderMenu);
+        validateOrderMenuContainBlank(orderMenu);
+        validateOrderMenuContainHyphen(orderMenu);
+    }
+    private void validateOrderMenuIsOverZero(String orderMenu) {
+        if (!orderMenu.contains(",")) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    public void validateOrderMenuContainBlank(String orderMenu){
-        if(orderMenu.contains(" ")){
+    private void validateOrderMenuContainBlank(String orderMenu) {
+        if (orderMenu.contains(" ")) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    public void validateOrderMenuContainHyphen(String orderMenu){
-        if(!orderMenu.contains("-")){
+    private void validateOrderMenuContainHyphen(String orderMenu) {
+        if (!orderMenu.contains("-")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void validateOrderMenuIsDuplicate(List<OrderRequest> orderList) {
+        List<String> menuNames = orderList.stream().map(OrderRequest::menu).toList();
+        Set<String> copyMenu = new HashSet<>(menuNames);
+        if (menuNames.size() != copyMenu.size()) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
