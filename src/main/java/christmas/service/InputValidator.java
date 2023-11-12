5bf31a2642b6e3928e46a881f96d88b0870a687b
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-13 01:24:52 +0900

    feat - 공백 "," "-" 관련 예외 추가

--- src/main/java/christmas/service/InputValidator.java
+++ src/main/java/christmas/service/InputValidator.java
@@ -14,7 +14,25 @@ public class InputValidator {
 
     public void validateInputDataInDateRange(int date) {
         if (date > 31 || date < 1) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void validateOrderMenuIsOverZero(String orderMenu){
+        if(!orderMenu.contains(",")){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void validateOrderMenuContainBlank(String orderMenu){
+        if(orderMenu.contains(" ")){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void validateOrderMenuContainHyphen(String orderMenu){
+        if(!orderMenu.contains("-")){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
