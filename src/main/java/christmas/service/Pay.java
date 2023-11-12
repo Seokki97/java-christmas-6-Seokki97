5ed74982136c1ab7e01333e2038cc26eba699dc2
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-12 23:57:10 +0900

    feat - 선물을 받을 수 있는지 검증 구현

--- src/main/java/christmas/service/Pay.java
+++ src/main/java/christmas/service/Pay.java
@@ -1,6 +1,8 @@
 package christmas.service;
 
 
+import christmas.domain.EventList;
+
 public class Pay {
 
     private static final int EVENT_PRICE = 120000;
@@ -10,8 +12,19 @@ public class Pay {
         this.totalPrice = totalPrice;
     }
 
+    public String validateCanGetGift() {
+        if (isTotalPayOverGiftEventPrice()) {
+            return EventList.CHAMPAGNE.getEventName();
+        }
+        return EventList.NOTHING.getEventName();
+    }
+
     public boolean isTotalPayOverGiftEventPrice() {
         return totalPrice > EVENT_PRICE;
+    }
+
+    public boolean isEventCondition() {
+        return totalPrice > 10000;
     }
 
     public int getTotalPay() {
