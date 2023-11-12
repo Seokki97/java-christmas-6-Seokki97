1aadfe15af0b366b11a919bb815ec6887aa6f562
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-12 23:04:29 +0900

    feat - 매직넘버 제거

--- src/main/java/christmas/service/CalendarService.java
+++ src/main/java/christmas/service/CalendarService.java
@@ -4,19 +4,25 @@ import java.util.Calendar;
 
 public class CalendarService {
 
+    private static final int YEAR = 2023;
+    private static final int WEEKEND = 6;
+    private static final int WEEK_UNIT = 7;
+    private static final int STAR_DAY = 3;
+    private static final int CHRISTMAS = 25;
+
     public int findVisitDayOfWeek(int visitedDay) {
         Calendar calendar = Calendar.getInstance();
 
-        calendar.set(2023, Calendar.DECEMBER, visitedDay);
+        calendar.set(YEAR, Calendar.DECEMBER, visitedDay);
 
-        return calendar.get(Calendar.DAY_OF_WEEK); // 6 금 7 토
+        return calendar.get(Calendar.DAY_OF_WEEK);
     }
 
     public boolean isWeekend(int dayOfWeek) {
-        return dayOfWeek >= 6;
+        return dayOfWeek >= WEEKEND;
     }
 
     public boolean isStarDay(int visitedDay) {
-        return visitedDay % 7 == 3 || visitedDay == 25;
+        return visitedDay % WEEK_UNIT == STAR_DAY || visitedDay == CHRISTMAS;
     }
 }
