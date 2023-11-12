4a01a98ff0a5f7eb725b139017ace79ea6fdfa27
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-12 23:00:24 +0900

    feat - OrderController 구현

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,10 +1,12 @@
 package christmas.controller;
 
-import christmas.domain.Order;
-import christmas.domain.Pay;
+import camp.nextstep.edu.missionutils.Console;
+import christmas.dto.OrderRequest;
+import christmas.service.InputValidator;
 import christmas.service.OrderService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,23 +14,40 @@ public class OrderController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final OrderService orderService;
+
+    private final InputValidator inputValidator;
 
     public OrderController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.orderService = new OrderService();
+        this.inputValidator = new InputValidator();
+    }
+
+    public int readVisitDay() {
+        inputView.showVisitDay();
+        String inputData = Console.readLine();
+        inputValidator.validateInputDataIsNumber(inputData);
+        int date = Integer.parseInt(inputData);
+        inputValidator.validateInputDataInDateRange(date);
+        return date;
     }
 
     public List<String> readOrderMenuInformation() {
-        String data = inputView.readOrderMenu();
-        String[] menuInformation = data.split(",");
+        inputView.showOrderMenu();
+        String orderList = Console.readLine();
+        String[] menuInformation = orderList.split(",");
         return Arrays.stream(menuInformation).toList();
     }
 
-    public List<Order> receiveOrder() {
-        List<Order> orderList = orderService.generateOrderList(readOrderMenuInformation());
-        outputView.showOrderMenu(orderList);
-        return orderList;
+    public List<OrderRequest> generateOrderRequest(List<String> orderList) {
+        List<OrderRequest> orderRequests = new ArrayList<>();
+        for (String menu : orderList) {
+            String[] menuInformation = menu.split("-");
+            String menuName = menuInformation[0];
+            int orderCount = Integer.parseInt(menuInformation[1]);
+            orderRequests.add(new OrderRequest(menuName, orderCount));
+        }
+        return orderRequests;
     }
+
 }
