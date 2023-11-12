7852c56d394b486f0b1280917935d91ce340926d
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-13 02:58:34 +0900

    refactor - eventList 내의 비즈니스 로직 제거

--- src/main/java/christmas/domain/EventList.java
+++ src/main/java/christmas/domain/EventList.java
@@ -1,49 +1,22 @@
 package christmas.domain;
 
 public enum EventList {
-    WEEKDAY("평일 할인: ", -2023),
-    SUNDAY("주말 할인: ", -2023),
-    SPECIAL("특별 할인: ", -1000),
-    GIFT("증정 이벤트: ", 0),
-    NOTHING("해당 없음", 0);
+
+    D_DAY("크리스마스 디데이 할인: "),
+    WEEKDAY("평일 할인: "),
+    SUNDAY("주말 할인: "),
+    SPECIAL("특별 할인: "),
+    GIFT("증정 이벤트: "),
+    NOTHING("없음"),
+    CHAMPAGNE("샴페인 1개");
+
     private final String eventName;
 
-    private final int discountMoney;
-
-    EventList(String eventName, int discountMoney) {
+    EventList(String eventName) {
         this.eventName = eventName;
-        this.discountMoney = discountMoney;
     }
 
-    public static EventList findDayEvent(int dayOfWeek) {
-        if (dayOfWeek >= 5) {
-            return EventList.SUNDAY;
-        }
-        return EventList.WEEKDAY;
-    }
-
-    public static EventList isStarEvent(int day) {
-        if (day % 7 == 3) {
-            return EventList.SPECIAL;
-        }
-        if (day == 25) {
-            return EventList.SPECIAL;
-        }
-        return EventList.NOTHING;
-    }
-
-    public static EventList isGiftEvent(Pay pay) {
-        if (pay.isTotalPayOverGiftEventPrice()) {
-            return EventList.GIFT;
-        }
-        return EventList.NOTHING;
-    }
-
-    public int getDiscountMoney() {
-        return discountMoney;
-    }
-
-    public String getEventName(){
-     return eventName;
+    public String getEventName() {
+        return eventName;
     }
 }
