da058f859068784eace4d42a7fbab1f2e2d17f4b
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-13 23:03:57 +0900

    refactor - 메서드 분리

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,9 +1,12 @@
 package christmas.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Order;
 import christmas.dto.OrderRequest;
 import christmas.service.InputValidator;
+import christmas.service.OrderService;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,12 +14,15 @@ import java.util.List;
 public class OrderController {
 
     private final InputView inputView;
-
     private final InputValidator inputValidator;
+    private final OrderService orderService;
+    private final OutputView outputView;
 
-    public OrderController(InputView inputView) {
+    public OrderController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.inputValidator = new InputValidator();
+        this.orderService = new OrderService();
+        this.outputView = outputView;
     }
 
     public int readVisitDay() {
@@ -33,12 +39,12 @@ public class OrderController {
         return date;
     }
 
-    public List<OrderRequest> validateOrderMenuInformation() {
+    public List<OrderRequest> readOrderMenuInformation() {
         inputView.showOrderMenu();
         List<OrderRequest> orderRequests;
         while (true) {
             try {
-                List<String> orderMenu = readOrderMenuInformation();
+                List<String> orderMenu = validateOrderMenuInformation();
                 orderRequests = generateOrderRequest(orderMenu);
                 inputValidator.validateOrderMenuIsDuplicate(orderRequests);
                 break;
@@ -49,13 +55,20 @@ public class OrderController {
         return orderRequests;
     }
 
-    public List<String> readOrderMenuInformation() {
+    public List<Order> readOrderMenu() {
+        List<OrderRequest> orderRequests = readOrderMenuInformation();
+        List<Order> orders = orderService.generateOrderList(orderRequests);
+        outputView.showOrderMenu(orders);
+        return orders;
+    }
+
+    private List<String> validateOrderMenuInformation() {
         String menuInformation = Console.readLine();
         inputValidator.checkMenuDataIsWrong(menuInformation);
         return Arrays.stream(menuInformation.split(",")).toList();
     }
 
-    public List<OrderRequest> generateOrderRequest(List<String> orderList) {
+    private List<OrderRequest> generateOrderRequest(List<String> orderList) {
         List<OrderRequest> orderRequests = new ArrayList<>();
         for (String order : orderList) {
             String[] menuInformation = inputValidator.checkNameAndCount(order);
