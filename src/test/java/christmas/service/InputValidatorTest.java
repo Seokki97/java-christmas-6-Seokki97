8933a88cb48cdf2f12ce63455c9a639e88aacab1
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-14 18:53:22 +0900

    test - 주문 내역에 음료만 있을 경우 예외 발생

--- src/test/java/christmas/service/InputValidatorTest.java
+++ src/test/java/christmas/service/InputValidatorTest.java
@@ -1,6 +1,9 @@
 package christmas.service;
 
+import christmas.domain.Order;
+import christmas.domain.OrderList;
 import christmas.dto.OrderRequest;
+import christmas.repository.MenuRepository;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,15 +12,6 @@ import org.junit.jupiter.api.Test;
 public class InputValidatorTest {
 
     private final InputValidator inputValidator = new InputValidator();
-
-    /*
-    void setInputValidator() {
-        inputValidator.checkOrderMenuOnlyDrinkType();
-        inputValidator.validateOrderMenuIsDuplicate();
-        inputValidator.checkMenuDataIsWrong();
-        inputValidator.checkNameAndCount();
-        inputValidator.validateVisitDay();
-    }*/
 
     @DisplayName("날짜 입력이 잘못됐을 경우 예외를 발생시킨다.")
     @Test
@@ -70,6 +64,21 @@ public class InputValidatorTest {
         Assertions.assertAll(
                 () -> Assertions.assertThrows(IllegalArgumentException.class,
                         () -> inputValidator.validateOrderMenuIsDuplicate(orderRequest))
+        );
+    }
+
+    @DisplayName("주문한 음식이 음료타입만 있을 경우 예외를 발생시킨다.")
+    @Test
+    void checkIsOnlyDrink(){
+        List<Order> orders = List.of(
+                new Order(MenuRepository.RED_WINE,1),
+                new Order(MenuRepository.ZERO_COKE,2)
+        );
+        OrderList orderList = new OrderList(orders);
+
+        Assertions.assertAll(
+                ()-> Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> inputValidator.checkOrderMenuOnlyDrinkType(orderList))
         );
     }
 }
