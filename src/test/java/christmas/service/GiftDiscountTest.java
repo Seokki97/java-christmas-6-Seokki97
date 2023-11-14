0e3ada4d6ba1e57cc375900cdf76cc45c1af81ed
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-14 19:37:00 +0900

    test - 증정 이벤트가 적용되는지 검증한다.

---
+++ src/test/java/christmas/service/GiftDiscountTest.java
@@ -0,0 +1,25 @@
+package christmas.service;
+
+import christmas.domain.EventList;
+import christmas.domain.Pay;
+import christmas.service.discount.GiftDiscount;
+import christmas.service.discount.SpecialDiscount;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GiftDiscountTest {
+
+    @DisplayName("증정 할인이 적용되는지 검증한다.")
+    @Test
+    void validateGiftEvent(){
+        GiftDiscount giftDiscount = new GiftDiscount(new Pay(120000));
+        Map<EventList, Integer> discountList = new HashMap<>();
+        giftDiscount.calculateDiscount(discountList);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(discountList.get(EventList.GIFT),25000)
+        );
+    }
+}
