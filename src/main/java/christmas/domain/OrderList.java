bc73f290d6750acf1d352997ce81ed3962bbe5c3
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-14 12:17:45 +0900

    feat - OrderList 구현

---
+++ src/main/java/christmas/domain/OrderList.java
@@ -0,0 +1,32 @@
+package christmas.domain;
+
+import christmas.dto.OrderRequest;
+import christmas.repository.MenuRepository;
+import java.util.Collections;
+import java.util.List;
+
+public record OrderList(List<Order> orderList) {
+
+    private static final int ZERO = 0;
+
+    public OrderList generateOrderList(List<OrderRequest> orderRequests) {
+        for (OrderRequest orderRequest : orderRequests) {
+            MenuRepository item = MenuRepository.findMenuByOrderItem(orderRequest.menu());
+            orderList.add(new Order(item, orderRequest.orderCount()));
+        }
+        return new OrderList(orderList);
+    }
+
+    public int calculateTotalPrice() {
+        int sum = ZERO;
+        for (Order order : orderList) {
+            sum += order.calculatePrice();
+        }
+        return sum;
+    }
+
+    @Override
+    public List<Order> orderList() {
+        return Collections.unmodifiableList(orderList);
+    }
+}
