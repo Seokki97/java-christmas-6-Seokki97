f4119b9b4fb90c36e14bf2462c1c39d9f6887588
JunSeok Kim <skaksdl1238@gmail.com>
2023-11-14 16:04:42 +0900

    test - Gift 항목을 제외한 혜택금액 계산

--- src/test/java/christmas/domain/DiscountTest.java
+++ src/test/java/christmas/domain/DiscountTest.java
@@ -3,6 +3,7 @@ package christmas.domain;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +11,24 @@ public class DiscountTest {
 
     private Discount discount;
 
-    @DisplayName("혜택 금액 총합을 계산한다.")
-    @Test
-    void calculateTotalBenefitMoney(){
+    @BeforeEach
+    void set(){
         Map<EventList, Integer> map = new HashMap<>();
 
         map.put(EventList.GIFT,25000);
         map.put(EventList.SUNDAY,2023);
         discount = new Discount(map);
+    }
+    @DisplayName("혜택 금액 총합을 계산한다.")
+    @Test
+    void calculateTotalBenefitMoney(){
 
         Assertions.assertEquals(27023,discount.getTotalBenefitMoney());
+    }
+
+    @DisplayName("Gift 항목을 제외한 혜택금액의 총합을 계산한다")
+    @Test
+    void calculateBenefitsExceptGift(){
+        Assertions.assertEquals(2023,discount.calculateTotalDiscountMoneyExceptGift());
     }
 }
